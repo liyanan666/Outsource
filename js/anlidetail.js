9db2bc8c406c11d170244bf10269a9598fb7fca6
liyanan666 <775055297@qq.com>
2017-12-14 11:43:48 +0800

    submit

--- js/anlidetail.js
+++ js/anlidetail.js
@@ -45,6 +45,32 @@ $(function(){
 			anli1:['art1_1.png','art1_2.png','art1_3.png','art1_4.png','art1_5.png','art1_6.png'],
 			anli2:['art2_1.png','art2_2.png','art2_3.png','art2_4.png'],
 			anli3:['art3_1.png','art3_2.png','art3_3.png','art3_4.png','art3_5.png','art3_6.png','art3_7.png','art3_8.png','art3_9.png','art3_10.png','art3_11.png','art3_12.png']
+		},
+		cartoon:{
+			title:"二维动画设计",
+			titleEngilsh:"TWO - DIMENSINAL ANIMATION DESIGN",
+			anli1:['manhua1.png','manhua2.png','manhua3.png','manhua4.png','manhua5.png','manhua6.png','manhua7.png','manhua8.png','manhua9.png','manhua10.png','manhua11.png','manhua12.png'],
+			anli2:['manhua2_1.png','manhua2_2.png','manhua2_3.png','manhua2_4.png','manhua2_5.png','manhua2_6.png','manhua2_7.png','manhua2_8.png','manhua2_9.png','manhua2_10.png','manhua2_11.png','manhua2_12.png','manhua2_13.png','manhua2_14.png','manhua2_15.png','manhua2_16.png'],
+			anli3:['manhua3_1.png','manhua3_2.png','manhua3_3.png','manhua3_4.png','manhua3_5.png','manhua3_6.png','manhua3_7.png']
+		},
+		dimensional:{
+			title:"三维动画设计",
+			titleEngilsh:"3D ANIMATION DESIGN",
+			anli1:['dimensional1_1.png','dimensional1_2.png','dimensional1_3.png','dimensional1_4.png','dimensional1_5.png','dimensional1_6.png','dimensional1_7.png'],
+			anli2:['dimensional2_1.png','dimensional2_2.png','dimensional2_3.png','dimensional2_4.png','dimensional2_5.png','dimensional2_6.png','dimensional2_7.png','dimensional2_8.png','dimensional2_9.png','dimensional2_10.png']
+		},
+		interactive:{
+			title:"交互设计",
+			titleEngilsh:"INTERACTIVE DESIGN",
+			anli1:['interactive1_1.png','interactive1_2.png','interactive1_3.png','interactive1_4.png','interactive1_5.png','interactive1_6.png','interactive1_7.png','interactive1_8.png','interactive1_9.png','interactive1_10.png','interactive1_11.png','interactive1_12.png','interactive1_13.png'],
+			anli2:['interactive2_1.png','interactive2_2.png','interactive2_3.png','interactive2_4.png','interactive2_5.png','interactive2_6.png','interactive2_7.png']
+		},
+		movie:{
+			title:"影视后期设计",
+			titleEngilsh:"FILM AND TELEVISION DESIGN",
+			anli1:['movie1_1.png','movie1_2.png','movie1_3.png','movie1_4.png','movie1_5.png','movie1_6.png'],
+			anli2:['movie2_1.png','movie2_2.png','movie2_3.png','movie2_4.png','movie2_5.png'],
+			anli3:['movie3_1.png','movie3_2.png','movie3_3.png','movie3_4.png','movie3_5.png','movie3_6.png','movie3_7.png']
 		}
 	}
 	
